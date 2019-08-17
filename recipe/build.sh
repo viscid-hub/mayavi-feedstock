7c00848425724e4ba6e9a8d962575004ac19d954
Kristofor Maynard <kristofor.maynard@gmail.com>
2019-08-17 17:01:29 -0400

    linux fix attempt 1

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ if [ "$(uname -s)" == "Darwin" ]; then
   $PYTHON setup.py install --single-version-externally-managed --record record.txt
 else
   USERNAME=$(id -u -n)
-  Xvfb :1 -screen 0 1280x1024x24 -auth localhost &
-  export DISPLAY=:1
+  # Xvfb :1 -screen 0 1280x1024x24 -auth localhost &
+  # export DISPLAY=:1
   $PYTHON setup.py install --single-version-externally-managed --record record.txt
-  killall -u $USERNAME Xvfb || true
+  # killall -u $USERNAME Xvfb || true
 fi
