79264248f71308984e612a48ea384f165226235f
Kristofor Maynard <kristofor.maynard@gmail.com>
2019-08-17 15:01:44 -0400

    Initial feedstock commit with conda-smithy 3.4.1.

---
+++ recipe/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$(uname -s)" == "Darwin" ]; then
+  $PYTHON setup.py install --single-version-externally-managed --record record.txt
+else
+  USERNAME=$(id -u -n)
+  Xvfb :1 -screen 0 1280x1024x24 -auth localhost &
+  export DISPLAY=:1
+  $PYTHON setup.py install --single-version-externally-managed --record record.txt
+  killall -u $USERNAME Xvfb || true
+fi
